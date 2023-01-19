036d9e26fed9bb2ffc0ceaa21573adaea7a35561
beOk91 <be_ok91@naver.com>
2023-01-19 14:32:35 +0900

    조회 반영

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Table } from 'antd';
+import React,{useState} from 'react';
+import { Table, Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 const columns = [
     {
         title: 'Id',
-        dataIndex: 'Id',
+        dataIndex: 'memberSeq',
         width: '10%'
     },
     {
         title: 'UserName',
-        dataIndex: 'UserName',
+        dataIndex: 'memberId',
         width: '20%'
     },
     {
-        title: 'columns1',
-        dataIndex: 'columns1',
+        title: '이름',
+        dataIndex: 'name',
     },
     {
         title: 'columns2',
@@ -30,15 +32,29 @@ const columns = [
     }
 ];
 
-const datasource = {};
+
 
 const UserList = () => {
+    const [dataSource,setDataSource] = useState([]);
+    const retrieveUser = async ()=>{
+        try{
+            const response = await axios.get('/user/list', {withCredentials: true });
+            setDataSource(response.data);
+        }catch(err){
+            console.log(err);
+        }
+    }
     return (
-        <Table
-            columns={columns}
-            datasource={datasource}
-            rowKey="Id"
-        />
+        <>
+            <Button type="primary" onClick={retrieveUser} icon={<SearchOutlined />}>
+            조회
+            </Button>
+            <Table
+                columns={columns}
+                dataSource={dataSource}
+                rowKey="Id"
+            />
+        </>
     )
 }
 export default UserList;
